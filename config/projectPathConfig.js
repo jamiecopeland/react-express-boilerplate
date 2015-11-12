8df4f3b74a6f9ae3f7492ae90c9575535677e7f0
Jamie Copeland <jamie@jamiecopeland.com>
2015-11-12 17:42:48 +0000

    Switched public folder from build to public

--- config/projectPathConfig.js
+++ config/projectPathConfig.js
@@ -2,5 +2,5 @@ import path from 'path';
 
 export const ROOT_PATH = path.normalize(path.join(__dirname, '/../'));
 export const SRC_PATH = path.join(ROOT_PATH, 'src');
-export const BUILD_PATH = path.join(ROOT_PATH, 'build');
+export const BUILD_PATH = path.join(ROOT_PATH, 'public');
 export const NODE_MODULES_PATH = path.join(ROOT_PATH, 'node_modules');
