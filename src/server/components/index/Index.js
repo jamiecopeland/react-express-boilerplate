e935932c7f3d4044f8a116bb25459f19c5f2d168
Jamie Copeland <jamie@jamiecopeland.com>
2016-02-04 16:29:57 +0000

    Tidy Index.js and remove Index.scss

--- src/server/components/index/Index.js
+++ src/server/components/index/Index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
-// import './Index.scss';
+function renderJsInclude(jsPaths) {
+  return jsPaths.map((jsPath) => (
+    <script key={jsPath} src={jsPath} />
+  ))
+}
+
+function renderCssInclude(cssPaths) {
+  return cssPaths ? cssPaths.map((cssPath) => (
+       <link key={cssPath} rel="stylesheet" href={cssPath}/>
+     )) : null;
+}
 
 export default class Index extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
-
-  renderJsInclude(jsPaths) {
-    return jsPaths.map((jsPath) => (
-      <script key={jsPath} src={jsPath} />
-    ))
-  }
-
-  renderCssInclude(cssPaths) {
-    return cssPaths ? cssPaths.map((cssPath) => (
-         <link key={cssPath} rel="stylesheet" href={cssPath}/>
-       )) : null;
-  }
+  static propTypes = {
+    title: React.PropTypes.string,
+    jsPaths: React.PropTypes.array,
+    cssPaths: React.PropTypes.array,
+    internalCSS: React.PropTypes.string
+  };
 
   render() {
     return (
@@ -29,21 +30,14 @@ export default class Index extends React.Component {
           <style rel="stylesheet">
             {this.props.internalCSS}
           </style>
-          {this.renderCssInclude(this.props.cssPaths)}
+          {renderCssInclude(this.props.cssPaths)}
         </head>
         <body>
           <div id="app-dom-hook"></div>
-          {this.renderJsInclude(this.props.jsPaths)}
+          {renderJsInclude(this.props.jsPaths)}
         </body>
       </html>
     );
   }
 
 }
-
-Index.propTypes = {
-  title: React.PropTypes.string,
-  jsPaths: React.PropTypes.array,
-  cssPaths: React.PropTypes.array,
-  internalCSS: React.PropTypes.string
-};
